3d376838497f16b63fbb61a129b1995e707c691d
bala-amypo <bala@amypo.in>
2025-12-16 09:34:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++ src/main/java/com/example/demo/entity/Stuentity.java
@@ -1,1 +1,11 @@
-package
+package com.example.demo.entity;
+
+import jarkata.persistence.Entity;
+import jarkata.persistence.GeneratedValue;
+import jarkata.persistence.Id;
+
+@Entity
+public class Stuentity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+}
