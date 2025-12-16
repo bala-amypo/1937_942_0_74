af4492f3146676451221d261def5f082d1227dea
bala-amypo <bala@amypo.in>
2025-12-16 09:39:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++ src/main/java/com/example/demo/entity/Stuentity.java
@@ -8,4 +8,19 @@ import jarkata.persistence.Id;
 public class Stuentity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private float cgpa;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName()
 }
